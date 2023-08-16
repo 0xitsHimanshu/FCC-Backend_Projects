1022effd8dd8acb265ffa33853f5ea4bde199680
Himanshu <kyadav.9643@gmail.com>
2023-08-16 13:32:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,35 @@ app.get("/api/timestamp", function (req, res) {
   res.json({ unix: Date.now(), utc: Date() });
 });
 
+app.get("/api", (req, res)=>{
+    const currentDate = new Date().toUTCString;
+    const currentUnix = Date.parse(currentDate)
+
+    res.json({unix: currentUnix, utc: currentDate});
+})
+
 app.get("/api/:date", (req, res)=>{
-    //time stamp
+    const dateString = req.params.date;
+    const dateStringRegex = /^[0-9]+$/;
+    const numbersOnly = dateStringRegex.test(dateString);
+
+    if(!numbersOnly){
+        const unixTimestamp = Date.parse(dateString);
+        const utcDate = new Date(unixTimestamp).toUTCString;
+
+        unixTimestamp
+        ? res.json({unix:unixTimestamp , utc : utcDate})
+        : res.json({error: "Invalid Date"})
+
+    } else {
+        const unixTimestamp = parseInt(dateString);
+        const utcDate = new Date(unixTimestamp).toUTCString;
+        
+        res.json({unix: unixTimestamp, utc: utcDate});
+    }
 })
 
 // listen for requests :)
-var listener = app.listen(port, function () {
+const listener = app.listen(port, function () {
   console.log(`Your server is succesfully running on http://localhost:${port}`);
 });
