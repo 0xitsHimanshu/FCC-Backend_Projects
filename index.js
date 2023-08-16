b30d0b6577616d491f3eb853e4978a3e72268429
Himanshu <kyadav.9643@gmail.com>
2023-08-16 12:27:10 +0530

    added index.js
    After intializing the express , builted the test api endpoint which is /api/timestamp, working absolutely fine now

---
+++ index.js
@@ -0,0 +1,35 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+const cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/timestamp", function (req, res) {
+  res.json({ unix: Date.now(), utc: Date() });
+});
+
+app.get("/api/:date", (req, res)=>{
+    //time stamp
+})
+
+// listen for requests :)
+var listener = app.listen(port, function () {
+  console.log(`Your server is succesfully running on http://localhost:${port}`);
+});
