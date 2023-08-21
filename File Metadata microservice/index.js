632867facccdd130bf0d2e38a3c1dcfea17b18fb
Himanshu <kyadav.9643@gmail.com>
2023-08-21 23:26:03 +0530

    Update index.js

---
+++ File Metadata microservice/index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse',upload.single('upfile'), ( req, res )=>{
+  try{
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+  } catch(err) {
+    res.send(400);
+  }
+})
+
+app.get('/api/greeting', (req, res)=>{
+  res.json({
+    greeting: 'Hello'
+  });
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
+s
